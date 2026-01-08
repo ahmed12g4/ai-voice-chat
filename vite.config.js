aea27c7b5e87c13303b8f07952c8d96e3e70fa29
Ahmed Seleem <ahmedalaaseleem989@gmail.com>
2026-01-09 01:36:59 +0200

    Final fix for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // تأكد من هذا
   build: {
     outDir: "dist",
-    rollupOptions: {
-      output: {
-        // تأكد من الملفات تبقى .js
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
   },
-  server: {
-    headers: {
-      "Content-Type": "application/javascript; charset=utf-8",
-    },
-  },
+  base: "./", // مهم جداً
 });
